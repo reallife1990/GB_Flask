c008bdde6794153ccdb2899db7c58096eb7ca393
Pavel <reallife1990msk@mail.ru>
2023-03-21 23:47:46 +0300

    hw_09

--- blog/extensions.py
+++ blog/extensions.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
 from flask_admin import Admin
 from blog.admin.views import CustomAdminIndexView
+from flask_combo_jsonapi import Api
 
 
 login_manager = LoginManager()
@@ -13,3 +14,4 @@ csrf = CSRFProtect()
 admin = Admin(index_view=CustomAdminIndexView(),
     name='Blog Admin Panel',
     template_mode='bootstrap4',)
+api = Api()
